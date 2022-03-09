9806c1762ec743a85a317d122f7058a517c3107a
katie <katiemiles12345@gmail.com>
2022-03-09 14:56:20 +0000

    K&G: adding tests to examine the function of the cache

--- src/test/java/ic/doc/CacheTest.java
+++ src/test/java/ic/doc/CacheTest.java
@@ -1,4 +1,19 @@
 package ic.doc;
 
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Test;
+import org.junit.Rule;
+
+
 public class CacheTest {
+
+  @Rule public JUnitRuleMockery context = new JUnitRuleMockery();
+
+
+  @Test
+  public void storesTemperatureInCache(){
+
+  }
+
+
 }
